e1f019ec6b25a83cb37071c10def100284ef145c
Lymei <ronaldofelipespm@bol.com.br>
2023-03-07 06:08:23 -0300

    Codificador de Texto

--- basics/codificador_de_texto.py
+++ basics/codificador_de_texto.py
@@ -1,4 +1,6 @@
-import random
+import random, pyperclip, os
+
+clear = lambda: os.system('cls')
 
 def codification(key,letters,text):
     letters_length = len(letters)
@@ -12,7 +14,7 @@ def codification(key,letters,text):
 
         while found == False and alphabet_index < letters_length:
             if letters[alphabet_index] == new_text[letter_index]:
-                final_text += letters[(alphabet_index+key)%letters_length]
+                final_text += letters[(alphabet_index+(key*(letter_index+1)))%letters_length]
                 found = True
             alphabet_index += 1
         
@@ -33,7 +35,7 @@ def decoding(key,letters,text):
 
         while found == False and alphabet_index < letters_length:
             if letters[alphabet_index] == new_text[letter_index]:
-                final_text += letters[(alphabet_index-key)%letters_length]
+                final_text += letters[(alphabet_index-(key*(letter_index+1)))%letters_length]
                 found = True
             alphabet_index += 1
         
@@ -43,29 +45,37 @@ def decoding(key,letters,text):
     return final_text
 
 def code_main(letters):
-    text = input('\nDigite uma frase: ')
+    clear()
+    text = input('Digite uma frase: ')
 
     key = random.randrange(10000)
 
-    print(f'O código de decodificação é {key}')
+    pyperclip.copy(key)
+    print(f'\nO código de decodificação foi copiado.')
 
     coded = codification(key, letters, text)
 
     print(f'\nSua mensagem codificada é [{coded}]\n')
+    print('Aperte enter para continuar...')
+    input()
 
 def decode_main(letters):
-    text = input('\nDigite uma frase: ')
+    clear()
+    text = input('Digite uma frase: ')
 
-    key = int(input('Digite o código de decodificação: '))
+    key = int(input('\nDigite o código de decodificação: '))
 
     decoded = decoding(key, letters, text)
 
-    print(f'\nSua mensagem decodificada é [{decoded}]\n')
+    print(f'\nSua mensagem decodificada é [{decoded}].\n')
+    print('Aperte enter para continuar...')
+    input()
 
 def menu():
     response = 0
 
     while response != '1' and response != '2' and response != '3':
+        clear()
         print('Pressione 1(um) para codificar.')
         print('Pressione 2(dois) para decodificar.')
         print('Pressione 3(três) para sair.\n')
@@ -75,7 +85,7 @@ def menu():
 
 def main():
 
-    letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',' ','!','?','@','*','%','&','/','.',',','<','>',')','(',']','[','~','^','`',]
+    letters = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','0','1','2','3','4','5','6','7','8','9',' ','!','?','@','$','#','*','%','&','/','.',':',',',';','<','>',')','(',']','[','}','{','~','^','`','"',"'",'+','-','=','_','|']
 
     response = 0
     while response != '3':
